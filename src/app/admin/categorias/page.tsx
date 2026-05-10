33af500c3fd717c2618f94f045cfbaac2459c9c6
erickmd79 <erickmd79@users.noreply.github.com>
2026-05-10 00:09:29 -0600

    Update page.tsx

--- src/app/admin/categorias/page.tsx
+++ src/app/admin/categorias/page.tsx
@@ -49,6 +49,30 @@ export default function CategoriasPage() {
   }
 
   return (
+    <div className="flex gap-4 mb-8">
+
+  <a
+    href="/admin"
+    className="bg-[#20B8C9] text-white px-6 py-3 rounded-2xl font-bold"
+  >
+    Pedidos
+  </a>
+
+  <a
+    href="/admin/productos"
+    className="bg-[#F7AFAF] text-white px-6 py-3 rounded-2xl font-bold"
+  >
+    Productos
+  </a>
+
+  <a
+    href="/admin/categorias"
+    className="bg-[#F6D36B] text-white px-6 py-3 rounded-2xl font-bold"
+  >
+    Categorías
+  </a>
+
+</div>
     <div className="p-10">
 
       <h1 className="text-5xl font-bold mb-10 text-cyan-700">
