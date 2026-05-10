98aa9bad75fd441d5bfccac46a94609256d669fd
erickmd79 <erickmd79@users.noreply.github.com>
2026-05-09 22:00:32 -0600

    subida imagen productos

--- src/app/pedido/page.tsx
+++ src/app/pedido/page.tsx
@@ -9,6 +9,7 @@ export default function Page() {
   const [telefono, setTelefono] = useState("")
   const [fecha, setFecha] = useState("")
   const [notas, setNotas] = useState("")
+  const [imagen, setImagen] = useState<File | null>(null)
 
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("carrito") || "[]")
@@ -22,6 +23,26 @@ export default function Page() {
 
   // 🔥 FUNCIÓN PARA GUARDAR EN SUPABASE
  const guardarPedido = async (pedido: any) => {
+   let imagenUrl = ""
+   if (imagen) {
+
+  const nombreArchivo =
+    `${Date.now()}-${imagen.name}`
+
+  const { error: uploadError } =
+    await supabase.storage
+      .from("productos")
+      .upload(nombreArchivo, imagen)
+
+  if (!uploadError) {
+
+    const { data } = supabase.storage
+      .from("productos")
+      .getPublicUrl(nombreArchivo)
+
+    imagenUrl = data.publicUrl
+  }
+}
   const { error } = await supabase.from("pedidos").insert([
     {
       cliente: pedido.cliente,
@@ -37,6 +58,7 @@ export default function Page() {
 
       total: pedido.total,
       estado: "pendiente",
+      imagen: imagenUrl,
     },
   ])
 
@@ -110,6 +132,22 @@ export default function Page() {
           onChange={(e) => setNotas(e.target.value)}
           className="w-full border p-2 rounded"
         />
+        <div className="mt-4">
+  <label className="block mb-2 font-semibold">
+    Imagen de referencia
+  </label>
+
+  <input
+    type="file"
+    accept="image/*"
+    onChange={(e) => {
+      if (e.target.files?.[0]) {
+        setImagen(e.target.files[0])
+      }
+    }}
+    className="w-full bg-white p-3 rounded-2xl border"
+  />
+</div>
       </div>
 
       {carrito.length === 0 ? (
