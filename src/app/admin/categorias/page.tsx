3ab194251279597eed226c37e4ab5bb1e9cfc95a
erickmd79 <erickmd79@users.noreply.github.com>
2026-05-10 13:42:17 -0600

    edit categorias

--- src/app/admin/categorias/page.tsx
+++ src/app/admin/categorias/page.tsx
@@ -25,6 +25,53 @@ export default function CategoriasAdmin() {
     }
   }
 
+  const eliminarCategoria = async (id: number) => {
+
+  const confirmar =
+    confirm("¿Eliminar categoría?")
+
+  if (!confirmar) return
+
+  const { error } = await supabase
+    .from("categorias")
+    .delete()
+    .eq("id", id)
+
+  if (error) {
+    alert("Error eliminando")
+    return
+  }
+
+  obtenerCategorias()
+}
+
+const editarCategoria = async (
+  id: number,
+  nombreActual: string
+) => {
+
+  const nuevoNombre =
+    prompt(
+      "Editar categoría",
+      nombreActual
+    )
+
+  if (!nuevoNombre) return
+
+  const { error } = await supabase
+    .from("categorias")
+    .update({
+      nombre: nuevoNombre
+    })
+    .eq("id", id)
+
+  if (error) {
+    alert("Error editando")
+    return
+  }
+
+  obtenerCategorias()
+}
   const guardarCategoria = async () => {
 
     if (!nombre) {
@@ -119,7 +166,40 @@ export default function CategoriasAdmin() {
               key={cat.id}
               className="p-4 rounded-2xl bg-[#FFF8F5] border border-[#F8D6D0]"
             >
-              {cat.nombre}
+              <div className="flex justify-between items-center">
+
+  <span>
+    {cat.nombre}
+  </span>
+
+  <div className="flex gap-2">
+
+    <button
+      onClick={() =>
+        editarCategoria(
+          cat.id,
+          cat.nombre
+        )
+      }
+      className="bg-[#BEE9E8]
+      px-4 py-2 rounded-xl"
+    >
+      Editar
+    </button>
+
+    <button
+      onClick={() =>
+        eliminarCategoria(cat.id)
+      }
+      className="bg-[#FFD6D6]
+      px-4 py-2 rounded-xl"
+    >
+      Eliminar
+    </button>
+
+  </div>
+
+</div>
             </div>
 
           ))}
